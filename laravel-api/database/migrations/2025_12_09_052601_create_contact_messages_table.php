7445c59c048045b15f71af0c6cdadff085fec1ba
prince6732 <prince.topntech@gmail.com>
2025-12-09 13:08:09 +0530

    commit

--- laravel-api/database/migrations/2025_12_09_052601_create_contact_messages_table.php
+++ laravel-api/database/migrations/2025_12_09_052601_create_contact_messages_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('phone_number');
+            $table->string('phone_number');
             $table->text('message');
             $table->boolean('is_read')->default(false);
             $table->timestamps();
